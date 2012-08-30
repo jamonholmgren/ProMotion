a07798831a110c08aea17060a0e8a1e21e68f4f4
Jamon Holmgren <jamon@clearsightstudio.com>
2012-08-30 16:20:08 -0700

    cc

--- lib/ProMotion/Screen.rb
+++ lib/ProMotion/Screen.rb
@@ -3,11 +3,9 @@ module ProMotion
     def open_with_nav_bar
       # Create UINavigationController, insert view
     end
-    
+
     def open
       # Push view onto existing UINavigationController
     end
-    
-    
   end
 end

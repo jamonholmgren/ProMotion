c9d58749b83cd91d9026e108b268b1cfe52b7d80
Silas J. Matson <silas@clearsightstudio.com>
2012-09-04 11:21:29 -0700

    Added helper to get content height.

---
+++ lib/ProMotion/helpers/MeasureHelper.rb
@@ -0,0 +1,13 @@
+module MeasureHelper
+  def content_height(view)
+    height = 0
+    view.subviews.each do |subView|
+      y = subView.frame.origin.y
+      h = subView.frame.size.height
+      if (y + h) > height
+        height = y + h
+      end
+    end
+    height
+  end
+end

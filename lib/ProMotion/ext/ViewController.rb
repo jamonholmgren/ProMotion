18da66734fbf709eaa81e84f88dfd8b4d0a4f95b
Jamon Holmgren <jamon@clearsightstudio.com>
2012-09-04 10:30:23 -0700

    Adding super to viewcontroller extensions

--- lib/ProMotion/ext/ViewController.rb
+++ lib/ProMotion/ext/ViewController.rb
@@ -3,14 +3,17 @@ module ProMotion
     attr_accessor :screen
 
     def viewDidLoad
+      super
       self.screen.view_did_load if self.screen && self.screen.respond_to?(:view_did_load)
     end
 
     def viewWillAppear(animated)
+      super
       self.screen.view_will_appear(animated) if self.screen && self.screen.respond_to?(:view_will_appear)
     end
 
     def viewDidAppear(animated)
+      super
       self.screen.view_did_appear(animated) if self.screen && self.screen.respond_to?(:view_did_appear)
     end
 

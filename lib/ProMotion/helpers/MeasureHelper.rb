9f742429abe8c7e8abb900a6282090cb2ff97449
Silas J. Matson <silas@clearsightstudio.com>
2012-09-27 10:48:17 -0700

    Removed debugging .puts in content_height.

--- lib/ProMotion/helpers/MeasureHelper.rb
+++ lib/ProMotion/helpers/MeasureHelper.rb
@@ -4,7 +4,6 @@ module ProMotion
       def content_height(view)
         height = 0
         view.subviews.each do |sub_view|
-          $stderr.puts sub_view
           next if sub_view.isHidden
           y = sub_view.frame.origin.y
           h = sub_view.frame.size.height

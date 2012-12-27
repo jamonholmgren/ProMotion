136fd117a2c7aea802d9a7d0efc0e8a7f433d199
Jamon Holmgren <jamon@clearsightstudio.com>
2012-12-26 18:42:46 -0800

    Removing accidental override

--- lib/ProMotion/screen/_table_screen_module.rb
+++ lib/ProMotion/screen/_table_screen_module.rb
@@ -4,19 +4,6 @@ module ProMotion
     include MotionTable::PlainTable
     include MotionTable::SearchableTable
 
-    def on_init
-      check_table_data_method
-
-      self.view = self.create_table_view_from_data(self.table_data)
-      if self.class.get_searchable
-        self.make_searchable(content_controller: self, search_bar: self.class.get_searchable_params)
-      end
-    end
-
-    def check_table_data_method
-      Console.log("- table_data method needed in table view screen.", with_color: Console::RED_COLOR) unless self.respond_to?(:table_data)
-    end
-
     def update_table_data
       self.update_table_view_data(table_data)
     end

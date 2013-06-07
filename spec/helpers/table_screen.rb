da9845d869a4c14b718e715ddd27771f9ef156a8
Mark Rickert <mjar81@gmail.com>
2013-06-07 17:13:37 -0400

    Update tableview helper spec to new paradigm while testing backwards compatability.

--- spec/helpers/table_screen.rb
+++ spec/helpers/table_screen.rb
@@ -40,36 +40,41 @@ class TestTableScreen < ProMotion::SectionedTableScreen
           accessory: :switch
         }, {
           title: "Switch With Action",
-          accessory: :switch,
-          accessory_action: :tap_counter
+          accessory: {
+              view: :switch,
+              action: :tap_counter
+            } ,
         }, {
           title: "Switch With Action And Parameters",
-          accessory: :switch,
-          accessory_action: :increment_counter_by,
-          arguments: { number: 3 }
+          accessory: {
+            view: :switch,
+            action: :increment_counter_by,
+            arguments: { number: 3 }
+          } ,
         }, {
           title: "Custom View",
           accessory: custom_accessory_view
         }, {
           title: "Custom View With Action",
-          accessory: custom_accessory_view,
-          accessory_action: :tap_counter
+          accessory:{
+            view: custom_accessory_view,
+            action: :tap_counter
+          },
         }, {
           title: "Custom View With Action And Parameters",
-          accessory: :switch,
-          accessory_action: :increment_counter_by,
-          arguments: { number: 3 }
+          accessory: {
+            view: :switch,
+            action: :increment_counter_by,
+            arguments: { number: 3 }
+          },
         }, {
           title: "Switch With Cell Tap, Switch Action And Parameters",
-          accessory: :switch,
-          accessory_action: :increment_counter_by, arguments: { number: 3 },
+          accessory:{
+            view: :switch,
+            action: :increment_counter_by,
+            arguments: { number: 3 },
+          },
           action: :increment_counter_by, accessory_arguments: { number: 10 }
-        }, {
-          title: "Switch With Cell Tap, Switch Action And No Parameters",
-          accessory: :switch,
-          accessory_action: :increment_counter_by,
-          arguments: { number: 3 },
-          action: :increment_counter_by
         }]
     }]
   end

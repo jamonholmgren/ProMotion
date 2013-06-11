b26042aba496afc8876f07376cc5d71ddfc48651
Mark Rickert <mjar81@gmail.com>
2013-06-11 11:22:42 -0400

    Better handling of the cell identifier building.

--- lib/ProMotion/screens/_tables/table_data.rb
+++ lib/ProMotion/screens/_tables/table_data.rb
@@ -87,7 +87,7 @@ module ProMotion
 
     def set_data_cell_defaults(data_cell)
       data_cell[:cell_style] ||= UITableViewCellStyleDefault
-      data_cell[:cell_identifier] ||= build_cell_identifier(data_cell)
+      data_cell[:cell_identifier] = build_cell_identifier(data_cell)
       data_cell[:cell_class] ||= ProMotion::TableViewCell
       data_cell
     end
@@ -157,21 +157,19 @@ module ProMotion
     end
 
     def build_cell_identifier(data_cell)
-      data_cell[:cell_identifier] ||= begin
-        ident = data_cell[:cell_identifier] || "Cell"
+      ident = data_cell[:cell_identifier] || "Cell"
 
-        unless data_cell[:accessory].nil?
-          if data_cell[:accessory][:view] == :switch
-            ident << "-switch"
-          elsif !data_cell[:accessory][:view].nil?
-            ident << "-accessory"
-          end
+      unless data_cell[:accessory].nil?
+        if data_cell[:accessory][:view] == :switch
+          ident << "-switch"
+        elsif !data_cell[:accessory][:view].nil?
+          ident << "-accessory"
         end
-        ident << "-subtitle" if data_cell[:subtitle]
-        ident << "-remoteimage" if data_cell[:remote_image]
-        ident << "-image" if data_cell[:image]
-        ident
       end
+      ident << "-subtitle" if data_cell[:subtitle]
+      ident << "-remoteimage" if data_cell[:remote_image]
+      ident << "-image" if data_cell[:image]
+      ident
     end
 
   end

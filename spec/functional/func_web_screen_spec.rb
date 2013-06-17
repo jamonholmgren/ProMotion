abcccaf9a210a560603338bd54b50d2ef8185773
Mark Rickert <mjar81@gmail.com>
2013-06-17 19:10:26 -0400

    Some awesome WebScreen tests.

---
+++ spec/functional/func_web_screen_spec.rb
@@ -0,0 +1,113 @@
+describe "ProMotion::TestWebScreen functionality" do
+  tests PM::TestWebScreen
+
+  # Override controller to properly instantiate
+  def controller
+    rotate_device to: :portrait, button: :bottom
+    @webscreen ||= TestWebScreen.new(nav_bar: true)
+    @webscreen.main_controller
+  end
+
+  after do
+    @webscreen = nil
+  end
+
+  it "should have the proper html content" do
+    file_name = "WebScreen.html"
+
+    @webscreen.set_content(file_name)
+
+    @loaded_file = File.read(File.join(NSBundle.mainBundle.resourcePath, file_name))
+    wait 0.5 do
+      @webscreen.html.should == @loaded_file
+    end
+  end
+
+  it "should allow you to navigate to a website" do
+    @webscreen.set_content(NSURL.URLWithString("http://www.google.com"))
+    wait 1.0 do
+      @webscreen.html.include?('<form action="/search"').should == true
+    end
+  end
+
+  # it "should have a navigation bar" do
+  #   @map.navigationController.should.be.kind_of(UINavigationController)
+  # end
+
+  # it "should have the map properly centered" do
+  #   center_coordinate = @map.center
+  #   center_coordinate.latitude.should == 35.090648651123
+  #   center_coordinate.longitude.should == -82.965972900391
+  # end
+
+  # it "should move the map center" do
+  #   @map.center = {latitude: 35.07496, longitude: -82.95916, animated: true}
+
+  #   wait 0.75 do
+  #     center_coordinate = @map.center
+  #     center_coordinate.latitude.should == 35.07496
+  #     center_coordinate.longitude.should == -82.95916
+  #   end
+  # end
+
+  # it "should select an annotation" do
+  #   @map.selected_annotations.should == nil
+  #   @map.select_annotation(@map.annotations.first, true)
+  #   wait 0.75 do
+  #     @map.selected_annotations.count.should == 1
+  #   end
+  # end
+
+  # it "should select another annotation and check that the title is correct" do
+  #   @map.selected_annotations.should == nil
+  #   @map.select_annotation(@map.annotations[1], true)
+  #   wait 0.75 do
+  #     @map.selected_annotations.count.should == 1
+  #   end
+
+  #   @map.selected_annotations.first.title.should == "Turtleback Falls"
+  #   @map.selected_annotations.first.subtitle.should == "Nantahala National Forest"
+
+  # end
+
+  # it "should deselect selected annotations" do
+  #   @map.select_annotation(@map.annotations.last, true)
+  #   wait 0.75 do
+  #     # @map.selected_annotations.count.should == 1
+  #   end
+
+  #   @map.deselect_annotations
+  #   wait 0.75 do
+  #     @map.selected_annotations.should == nil
+  #   end
+  # end
+
+  # it "should add an annotation and be able to zoom immediately" do
+  #   ann = {
+  #     longitude: -82.966093558105,
+  #     latitude: 35.092520895652,
+  #     title: "Something Else"
+  #   }
+  #   @map.annotations.count.should == 5
+  #   @map.add_annotation ann
+  #   @map.annotations.count.should == 6
+  #   @map.set_region @map.region(coordinate: @map.annotations.last.coordinate, span: [0.05, 0.05])
+  #   @map.select_annotation @map.annotations.last
+  # end
+
+  # it "should be able to overwrite all annotations" do
+  #   anns = [{
+  #     longitude: -122.029620,
+  #     latitude: 37.331789,
+  #     title: "My Cool Pin"
+  #   },{
+  #     longitude: -80.8498118 ,
+  #     latitude: 35.2187218,
+  #     title: "My Cool Pin"
+  #   }]
+  #   @map.annotations.count.should == 5
+  #   @map.add_annotations anns
+  #   @map.annotations.count.should == 2
+  # end
+
+end

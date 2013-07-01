399cfc9d449428a039d65055dc572c8ba0b11ca2
Jamon Holmgren <jamon@clearsightstudio.com>
2013-07-01 13:49:25 -0700

    Refactored table folder and modules
    * Moved formotion_screen into thirdparty
    * Simplified GroupedTable
    * Removed PM::PlainTable
    * Moved RefreshableTable and SearchableTable into
    PM::Table::<modulename>
    * Combined table.rb and table_screen_module.rb which were similarily
    focused
    * Removed extraneous/redundant module SectionedTableScreen (it's just a
    TableScreen)
    * Moved searchable/refreshable into /extensions folder
    
    All tests passing: 197 specifications (334 requirements), 0 failures, 0
    errors

--- lib/ProMotion/table/table_screen.rb
+++ lib/ProMotion/table/table_screen.rb
@@ -1,16 +1,6 @@
 module ProMotion
-  # You can inherit a table screen from any UITableViewController if you include TableScreenModule
-  # Just make sure to implement the Obj-C methods in cocoatouch/TableViewController.rb.
   class TableScreen < TableViewController
-    include ProMotion::TableScreenModule
-    # Includes PM::PlainTable already
-  end
-
-  class GroupedTableScreen < TableScreen
-    include ProMotion::GroupedTable
-  end
-
-  class SectionedTableScreen < TableScreen
-    include ProMotion::SectionedTable
+    include ProMotion::ScreenModule
+    include ProMotion::Table
   end
 end

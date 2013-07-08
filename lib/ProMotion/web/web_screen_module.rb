f7636ca6f32295e25b7165eb966603695d93c28c
ainame <s.namai.2012@gmail.com>
2013-07-09 01:41:51 +0900

    separete "set_content" method into static file loding and HTTP request.

--- lib/ProMotion/web/web_screen_module.rb
+++ lib/ProMotion/web/web_screen_module.rb
@@ -42,25 +42,23 @@ module ProMotion
     end
 
     def set_content(content)
-      if content.is_a? NSURL
-        initialize_with_url content
+      content_path = File.join(NSBundle.mainBundle.resourcePath, content)
+
+      if File.exists? content_path
+        content_string = File.read content_path
+        content_base_url = NSURL.fileURLWithPath NSBundle.mainBundle.resourcePath
+        
+        self.web.loadHTMLString(convert_retina_images(content_string), baseURL:content_base_url)
       else
-        content_path = File.join(NSBundle.mainBundle.resourcePath, content)
-
-        if File.exists? content_path
-          content_string = File.read content_path
-          content_base_url = NSURL.fileURLWithPath NSBundle.mainBundle.resourcePath
-
-          self.web.loadHTMLString(convert_retina_images(content_string), baseURL:content_base_url)
-        else
-          # We assume the user wants to load an arbitrary string into the web view
-          self.web.loadHTMLString(content, baseURL:nil)
-        end
+        # We assume the user wants to load an arbitrary string into the web view
+        self.web.loadHTMLString(content, baseURL:nil)
       end
     end
 
-    def initialize_with_url(url)
-      request = NSURLRequest.requestWithURL(url)
+    def open_url(url)
+      request = NSURLRequest.requestWithURL(
+        url.is_a?(NSURL) ? url : NSURL.URLWithString(url)
+      )
       web.loadRequest request
     end
 

89c76c5bfe39fe9bb1be157d0bfe5f4260b0b336
Jamon Holmgren <jamon@clearsightstudio.com>
2013-07-12 21:16:10 -0700

    Remove indexable per @ainame

--- lib/ProMotion/table/extensions/indexable.rb
+++ lib/ProMotion/table/extensions/indexable.rb
@@ -1,10 +1,6 @@
 module ProMotion
   module Table
     module Indexable
-      def make_indexable(params={})
-        @pm_indexable = true
-      end
-
       def index_from_section_titles
         @promotion_table_data.filtered ? nil : @promotion_table_data.sections.collect{ |section| section[:title][0] }
       end

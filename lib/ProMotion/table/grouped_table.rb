a7999477d023c514e7bd3ac132ad5a08aa3f476f
Jamon Holmgren <jamon@clearsightstudio.com>
2013-07-16 16:07:12 -0700

    Fixing issue #231

--- lib/ProMotion/table/grouped_table.rb
+++ lib/ProMotion/table/grouped_table.rb
@@ -1,7 +1,5 @@
 module ProMotion
   module GroupedTable
-    include ProMotion::Table
-    
     def table_style
       UITableViewStyleGrouped
     end
--- lib/ProMotion/table/grouped_table_screen.rb
+++ lib/ProMotion/table/grouped_table_screen.rb
@@ -1,6 +1,7 @@
 module ProMotion
   class GroupedTableScreen < TableViewController
     include ProMotion::ScreenModule
+    include ProMotion::Table
     include ProMotion::GroupedTable
   end
 end

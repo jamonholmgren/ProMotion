04d147ce676e9ab97605e65d2694bfc56683c63e
ainame <s.namai.2012@gmail.com>
2014-03-03 00:29:44 +0900

    fix test class definition of PM::WebScreen

--- spec/helpers/web_screen.rb
+++ spec/helpers/web_screen.rb
@@ -2,21 +2,22 @@ class TestWebScreen < PM::WebScreen
 
   title "WebScreen Test"
 
-  attr_accessor :load_started, :load_failed, :load_finished, :load_failed_error
+  # accesor for wait_change method which is testing helper
+  attr_accessor :is_load_started, :is_load_finished, :is_load_failed, :is_load_failed_error
 
+  # implementation of PM::WebScreen's hook
   def load_started
-    self.load_started = true
+    self.is_load_started = true
   end
 
   def load_finished
-    self.load_finished = true
+    self.is_load_finished = true
   end
 
   def load_failed(error)
     puts "Load Failed: #{error.localizedDescription}"
     puts error.localizedFailureReason
-    self.load_failed = true
-    self.load_failed_error = error
+    self.is_load_failed = true
+    self.is_load_failed_error = error
   end
-
 end

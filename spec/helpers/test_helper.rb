5e854263ba8692ea177d02c551b0811e9c7c6131
Jamon Holmgren <jamon@clearsightstudio.com>
2014-06-21 15:48:09 -0700

    Fixed issue #479 where a crash would occur on swipe left with no :editing_style set

--- spec/helpers/test_helper.rb
+++ spec/helpers/test_helper.rb
@@ -3,3 +3,30 @@ class TestHelper
     UIDevice.currentDevice.systemVersion.to_f >= 7.0
   end
 end
+
+def silence_warnings(&block)
+  warn_level = $VERBOSE
+  $VERBOSE = nil
+  begin
+    result = block.call
+  ensure
+    $VERBOSE = warn_level
+  end
+  result
+end
+
+silence_warnings do
+  module Bacon
+    if ENV['filter']
+      $stderr.puts "Filtering specs that match: #{ENV['filter']}"
+      RestrictName = Regexp.new(ENV['filter'])
+    end
+
+    if ENV['filter_context']
+      $stderr.puts "Filtering contexts that match: #{ENV['filter_context']}"
+      RestrictContext = Regexp.new(ENV['filter_context'])
+    end
+
+    Backtraces = false if ENV['hide_backtraces']
+  end
+end

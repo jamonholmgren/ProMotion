5cd5ea8f438223765111077870d425205272f543
Ryan Linton <ryanlntn@gmail.com>
2014-08-22 13:36:41 -0700

    Dedup the functional screens

--- app/test_screens/uiimage_title_screen.rb
+++ app/test_screens/uiimage_title_screen.rb
@@ -1,15 +1,3 @@
-class UIImageTitleScreen < PM::Screen
-  attr_accessor :button_was_triggered
-
+class UIImageTitleScreen < FunctionalScreen
   title_image UIImage.imageNamed('test.png')
-
-  def will_appear
-    self.button_was_triggered = false
-    add UILabel.alloc.initWithFrame([[ 10, 10 ], [ 300, 40 ]]),
-      text: "Label Here"
-  end
-
-  def triggered_button
-    self.button_was_triggered = true
-  end
 end

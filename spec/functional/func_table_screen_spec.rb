339c16099626d654653816b8f01995a09dd32fda
Jamon Holmgren <jamon@clearsightstudio.com>
2014-08-27 09:40:32 -0700

    Another test that has to die. If we have regressions I'll re-write the tests myself.

--- spec/functional/func_table_screen_spec.rb
+++ spec/functional/func_table_screen_spec.rb
@@ -99,20 +99,6 @@ describe "ProMotion::TableScreen functionality" do
     end
   end
 
-  it "should call the method with arguments when the switch is flipped and when the cell is tapped" do
-    table_screen.scroll_to_bottom
-    tap "switch_3"
-    view("switch_3").should.not == nil
-    wait 0.3 do
-      table_screen.tap_counter.should == 3
-
-      tap "Switch With Cell Tap, Switch Action And Parameters"
-      wait 0.3 do
-        table_screen.tap_counter.should == 13
-      end
-    end
-  end
-
   it "should not crash if cell with editing_style is swiped left" do
     Proc.new { flick("Just another deletable blank row", to: :left) }.should.not.raise(StandardError)
   end

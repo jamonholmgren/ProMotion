90b0dd6aa12d553cad8fd9fb4eec66fd2c707528
Jamon Holmgren <jamon@clearsightstudio.com>
2014-09-04 08:56:24 -0700

    Workaround for issue #539

--- lib/ProMotion/table/grouped_table_screen.rb
+++ lib/ProMotion/table/grouped_table_screen.rb
@@ -2,6 +2,7 @@ module ProMotion
   class GroupedTableScreen < TableViewController
     include ProMotion::ScreenModule
     include ProMotion::Table
+    include ProMotion::Table::Utils # Workaround until https://hipbyte.freshdesk.com/support/tickets/2086 is fixed.
     include ProMotion::GroupedTable
   end
 end

ae7ebd6c702a990c354c374cb15528a7e8651e27
Mark Rickert <mjar81@gmail.com>
2014-09-13 16:13:14 -0500

    Check to see if the param is a hash in table utils

--- lib/ProMotion/table/table_utils.rb
+++ lib/ProMotion/table/table_utils.rb
@@ -2,7 +2,7 @@ module ProMotion
   module Table
     module Utils
       def index_path_to_section_index(params)
-        if params[:index_path]
+        if params.is_a?(Hash) && params[:index_path]
           params[:section] = params[:index_path].section
           params[:index] = params[:index_path].row
         end

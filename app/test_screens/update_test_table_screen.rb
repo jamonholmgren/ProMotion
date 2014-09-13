94e5ed103f3f06fde42664b576b22f9d2d53088c
Mark Rickert <mjar81@gmail.com>
2014-09-13 17:47:25 -0500

    Passing tests for index_path updating of tables.

---
+++ app/test_screens/update_test_table_screen.rb
@@ -0,0 +1,28 @@
+class UpdateTestTableScreen < PM::TableScreen
+  def table_data; @table_data ||= []; end
+  def on_load
+    @table_data = [{cells: []}]
+    update_table_data
+  end
+
+  def make_more_cells
+    @table_data = [{cells: [{title: "Cell 1"},{title: "Cell 2"}]}]
+  end
+
+  def change_cells
+    @table_data = [{cells: [{title: "Cell A"},{title: "Cell B"}]}]
+  end
+
+  def first_cell_title
+    cell_title(0)
+  end
+
+  def second_cell_title
+    cell_title(1)
+  end
+
+  def cell_title(index)
+    ip = NSIndexPath.indexPathForRow(index, inSection:0)
+    table_view.cellForRowAtIndexPath(ip).textLabel.text
+  end
+end

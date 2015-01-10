6dc571400d3b0d8947ef4eedae78cdba3b13655b
Silas J. Matson <silas@clearsightstudio.com>
2015-01-09 16:19:02 -0800

    Allow NSAttributedString to be passed to the title.

--- spec/unit/screen_module_spec.rb
+++ spec/unit/screen_module_spec.rb
@@ -10,4 +10,9 @@ describe "PM::ScreenModule" do
     @subject.title.should == 'Test Title'
   end
 
+  it 'should allow a NSAttributedString' do
+    @subject.title = NSAttributedString.alloc.initWithString("Test Title")
+    @subject.title.string.should == "Test Title"
+  end
+
 end

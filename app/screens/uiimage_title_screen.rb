3723106d0ac996e907acc23cbd63268375d14c18
Jamon Holmgren <jamon@clearsightstudio.com>
2015-04-26 20:08:22 -0700

    Simplified the code quite a bit, added more general-purpose pm_live_watch

--- app/screens/uiimage_title_screen.rb
+++ app/screens/uiimage_title_screen.rb
@@ -1,4 +1,12 @@
 class UIImageTitleScreen < FunctionalScreen
   title_image UIImage.imageNamed('test.png')
 
+  def on_load
+
+  end
+
+  def on_live_reload
+    puts "Yo"
+  end
+
 end
--- lib/ProMotion/repl/repl.rb
+++ lib/ProMotion/repl/repl.rb
@@ -1,62 +1,62 @@
 if RUBYMOTION_ENV == "development"
   module Kernel
     def pm_live_screens(opts={})
+      @screen_timer.invalidate if @screen_timer
+      @screen_timer = nil
       if opts == false || opts.to_s.downcase == "off"
-        @live_reload_timer.invalidate if @live_reload_timer
-        @live_reload_timer = nil
         "Live reloading of PM screens is now off."
       else
-        @live_reload_debug = opts[:debug]
-        enable_pm_live_screens(opts[:interval] || 0.5)
+        @screen_timer = pm_live_watch("#{pm_app_root_path}/app/screens/**/*.rb", opts) do |reloaded_file, new_code|
+          screen_names = pm_parse_screen_names(new_code)
+          puts "Reloaded #{screen_names.join(", ")} #{screen}." if opts[:debug]
+          vcs = pm_all_view_controllers(UIApplication.sharedApplication.delegate.window.rootViewController)
+          puts "Found #{vcs.map(&:to_s).join(", ")}." if opts[:debug]
+          vcs.each do |vc|
+            if pm_is_in_ancestry?(vc, screen_names)
+              puts "Sending :on_live_reload to #{vc.inspect}." if opts[:debug]
+              vc.send(:on_live_reload) if vc.respond_to?(:on_live_reload)
+            end
+          end
+        end
+        "Live reloading of PM screens is now on."
       end
     end
 
-    private
-
-    def live_reload_timer_every(interval, &callback)
-      NSTimer.scheduledTimerWithTimeInterval(interval, target: callback, selector: 'call:', userInfo: nil, repeats: true)
-    end
-
-    def enable_pm_live_screens(interval)
+    def pm_live_watch(path_query, opts={}, &callback)
       # Get list of screen files
-      return unless root_path = NSBundle.mainBundle.infoDictionary["ProjectRootPath"]
+      puts path_query if opts[:debug]
+      live_file_paths = Dir.glob(path_query)
+      puts live_file_paths if opts[:debug]
 
-      path_query = "#{root_path}/app/screens/**/*.rb"
-      puts path_query if @live_reload_debug
-      screen_file_paths = Dir.glob(path_query)
-      puts screen_file_paths if @live_reload_debug
-
-      screens = screen_file_paths.inject({}) do |out, screen_path_file|
-        out[screen_path_file] = Time.now
+      live_files = live_file_paths.inject({}) do |out, live_file_path_file|
+        out[live_file_path_file] = Time.now
         out
       end
 
-      @live_reload_timer = live_reload_timer_every(interval) do
-        screen_changed = false
-        screens.each do |screen, modified_date|
-          if File.exist?(screen) && File.mtime(screen) > modified_date
-            pm_reload_screen(screen)
-            screens[screen] = File.mtime(screen)
+      live_reload_timer = pm_live_reload_timer_every(opts[:interval] || 0.5) do
+        live_file_changed = false
+        live_files.each do |live_file, modified_date|
+          if File.exist?(live_file) && File.mtime(live_file) > modified_date
+            new_code = File.read(live_file)
+            eval(new_code)
+            callback.call live_file, new_code
+            live_files[live_file] = File.mtime(live_file)
           end
         end
       end
 
-      "Live reloading of ProMotion screens is now on."
+      "Watching #{path_query}."
+      live_reload_timer
     end
 
-    def pm_reload_screen(screen)
-      fresh_code = File.read(screen)
-      eval(fresh_code)
-      screen_names = pm_parse_screen_names(fresh_code)
-      puts "Reloaded #{screen_names.join(", ")} #{screen}." if @live_reload_debug
-      vcs = pm_all_view_controllers(UIApplication.sharedApplication.delegate.window.rootViewController)
-      puts "Found #{vcs.map(&:to_s).join(", ")}." if @live_reload_debug
-      vcs.each do |vc|
-        if pm_is_in_ancestry?(vc, screen_names)
-          puts "Sending :on_live_reload to #{vc.inspect}." if @live_reload_debug
-          vc.send(:on_live_reload) if vc.respond_to?(:on_live_reload)
-        end
-      end
+    private
+
+    def pm_live_reload_timer_every(interval, &callback)
+      NSTimer.scheduledTimerWithTimeInterval(interval, target: callback, selector: 'call:', userInfo: nil, repeats: true)
+    end
+
+    def pm_app_root_path
+      NSBundle.mainBundle.infoDictionary["ProjectRootPath"]
     end
 
     # Very permissive. Might get unnecessary reloads. That's okay.

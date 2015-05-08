a76685d12c8c12a85542d95d60a4147b3910e57f
Mark Rickert <mjar81@gmail.com>
2015-05-08 16:27:37 -0500

    Adds :searchable to the whitelisted properties a cell can have.

--- lib/ProMotion/table/cell/table_view_cell_module.rb
+++ lib/ProMotion/table/cell/table_view_cell_module.rb
@@ -23,7 +23,7 @@ module ProMotion
 
     # TODO: Remove this in ProMotion 2.1. Just for migration purposes.
     def check_deprecated_styles
-      whitelist = [ :title, :subtitle, :image, :remote_image, :accessory, :selection_style, :action, :long_press_action, :arguments, :cell_style, :cell_class, :cell_identifier, :editing_style, :moveable, :search_text, :keep_selection, :height, :accessory_type, :style, :properties ]
+      whitelist = [ :title, :subtitle, :image, :remote_image, :accessory, :selection_style, :action, :long_press_action, :arguments, :cell_style, :cell_class, :cell_identifier, :editing_style, :moveable, :search_text, :keep_selection, :height, :accessory_type, :style, :properties, :searchable ]
       if (data_cell.keys - whitelist).length > 0
         PM.logger.deprecated("In #{self.table_screen.class.to_s}#table_data, you should set :#{(data_cell.keys - whitelist).join(", :")} in a `properties:` hash. See TableScreen documentation.")
       end

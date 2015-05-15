0e53046d49a45f92234a3e2147f198cb978b6715
Jamon Holmgren <jamon@clearsightstudio.com>
2015-05-15 09:06:04 -0700

    Fixed brittle web request specs (cc @markrickert)

--- spec/functional/func_web_screen_spec.rb
+++ spec/functional/func_web_screen_spec.rb
@@ -18,32 +18,35 @@ describe "ProMotion::TestWebScreen functionality" do
   it "should have the proper html content" do
     file_name = "WebScreen.html"
 
-    controller.set_content(file_name)
-
-    @loaded_file = File.read(File.join(NSBundle.mainBundle.resourcePath, file_name))
-    wait_for_change controller, 'is_load_finished' do
-      controller.html.delete("\n").should == @loaded_file.delete("\n")
+    controller.mock!(:load_finished) do
+      loaded_file = File.read(File.join(NSBundle.mainBundle.resourcePath, file_name))
+      controller.html.delete("\n").should == loaded_file.delete("\n")
+      resume
     end
+    controller.set_content(file_name)
+    wait_max 8 {}
   end
 
   it "should allow you to navigate to a website" do
     stub_request(:get, "https://www.google.com/").
       to_return(body: %q{Google! <form action="/search">%}, content_type: "text/html")
 
-    controller.open_url(NSURL.URLWithString("https://www.google.com/"))
-    wait_for_change controller, 'is_load_finished' do
-      controller.html.include?('<form action="/search"').should == true
+    controller.mock!(:load_finished) do
+      controller.html.should.include('<form action="/search"')
+      resume
     end
+    controller.open_url(NSURL.URLWithString("https://www.google.com/"))
+    wait_max 8 {}
   end
 
   it "should manipulate the webscreen contents with javascript" do
-    controller.set_content('<h1 id="cool">Something Cool</h1>')
-
-    wait_for_change controller, 'is_load_finished' do
+    controller.mock!(:load_finished) do
       controller.evaluate('document.getElementById("cool").innerHTML = "Changed"')
       controller.html.should =~ /<h1 id="cool">Changed<\/h1>/
+      resume
     end
-
+    controller.set_content('<h1 id="cool">Something Cool</h1>')
+    wait_max 8 {}
   end
 
 end

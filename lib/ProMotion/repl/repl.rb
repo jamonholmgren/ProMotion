ee9595578bd829880bf21b485ea35c7d205c2111
Jamon Holmgren <jamon@clearsightstudio.com>
2015-05-16 23:40:46 -0700

    Added live reloading views, refactored into nice class

--- lib/ProMotion/repl/repl.rb
+++ lib/ProMotion/repl/repl.rb
@@ -1,63 +1,39 @@
 if RUBYMOTION_ENV == "development"
+  puts "Type `pm_live` to enable ProMotion's live reload system."
   module Kernel
-    def pm_live_screens(opts={})
-      @screen_timer.invalidate if @screen_timer
-      @screen_timer = nil
+    def pm_live(opts={})
+      @screen_watcher.stop if @screen_watcher
+      @view_watcher.stop if @view_watcher
       if opts == false || opts.to_s.downcase == "off"
         "Live reloading of PM screens is now off."
       else
-        @screen_timer = pm_live_watch("app/screens/**/*.rb", opts) do |reloaded_file, new_code|
-          screen_names = pm_parse_screen_names(new_code)
-          puts "Reloaded #{screen_names.join(", ")} #{reloaded_file}." if opts[:debug]
+        @screen_watcher = LiveReloader.new("app/screens/**/*.rb", opts).watch do |reloaded_file, new_code, class_names|
           vcs = pm_all_view_controllers(UIApplication.sharedApplication.delegate.window.rootViewController)
-          puts "Found #{vcs.map(&:to_s).join(", ")}." if opts[:debug]
           vcs.each do |vc|
-            if pm_is_in_ancestry?(vc, screen_names)
+            if pm_is_in_ancestry?(vc, class_names)
               puts "Sending :on_live_reload to #{vc.inspect}." if opts[:debug]
               vc.send(:on_live_reload) if vc.respond_to?(:on_live_reload)
             end
           end
         end
-        "Live reloading of PM screens is now on."
-      end
-    end
 
-    def pm_live_watch(path_query, opts={}, &callback)
-      # Get list of screen files
-      puts path_query if opts[:debug]
-      live_file_paths = Dir.glob("#{pm_app_root_path}/#{path_query}")
-      puts live_file_paths if opts[:debug]
-
-      live_files = live_file_paths.inject({}) do |out, live_file_path_file|
-        out[live_file_path_file] = Time.now
-        out
-      end
-
-      live_reload_timer = pm_live_reload_timer_every(opts[:interval] || 0.5) do
-        live_file_changed = false
-        live_files.each do |live_file, modified_date|
-          if File.exist?(live_file) && File.mtime(live_file) > modified_date
-            new_code = File.read(live_file)
-            eval(new_code)
-            callback.call live_file, new_code
-            live_files[live_file] = File.mtime(live_file)
+        @view_watcher = LiveReloader.new("app/views/**/*.rb", opts).watch do |reloaded_file, new_code, class_names|
+          views = pm_all_views(UIApplication.sharedApplication.delegate.window)
+          views.each do |view|
+            if pm_is_in_ancestry?(view, class_names)
+              puts "Sending :on_live_reload to #{view.inspect}." if opts[:debug]
+              view.send(:on_live_reload) if view.respond_to?(:on_live_reload)
+            end
           end
         end
-      end
 
-      "Watching #{path_query}."
-      live_reload_timer
+        "Live reloading of screens and views is now on."
+      end
     end
+    alias_method :pm_live_screens, :pm_live
+
 
     private
-
-    def pm_live_reload_timer_every(interval, &callback)
-      NSTimer.scheduledTimerWithTimeInterval(interval, target: callback, selector: 'call:', userInfo: nil, repeats: true)
-    end
-
-    def pm_app_root_path
-      NSBundle.mainBundle.infoDictionary["ProjectRootPath"]
-    end
 
     # Very permissive. Might get unnecessary reloads. That's okay.
     def pm_is_in_ancestry?(vc, screen_names)
@@ -69,13 +45,6 @@ if RUBYMOTION_ENV == "development"
       end
     end
 
-    def pm_parse_screen_names(code)
-      code.split("\n").map do |code_line|
-        matched = code_line.match(/^\s*class\s+(\S+)\s+</)
-        matched[1] if matched
-      end.compact
-    end
-
     def pm_all_view_controllers(root_view_controller)
       vcs = [ root_view_controller ]
       if root_view_controller.respond_to?(:viewControllers)
@@ -85,6 +54,12 @@ if RUBYMOTION_ENV == "development"
         vcs = vcs + root_view_controller.childViewControllers.map{|vc| pm_all_view_controllers(vc) }
       end
       vcs.flatten.uniq
+    end
+
+    def pm_all_views(root_view)
+      views = [ root_view ]
+      views = views + views.map{|v| v.subviews.map{|sv| pm_all_views(sv) } }
+      views.flatten.uniq
     end
   end
 end

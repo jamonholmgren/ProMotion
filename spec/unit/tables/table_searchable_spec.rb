4f95eec6d059549650b297da658d4cedbe283325
Mark Rickert <mjar81@gmail.com>
2015-06-18 14:23:37 -0400

    Move no results test to unit

--- spec/unit/tables/table_searchable_spec.rb
+++ spec/unit/tables/table_searchable_spec.rb
@@ -24,4 +24,18 @@ describe "table screen searchable functionality" do
     screen.on_load
     screen.tableView.contentOffset.should == CGPointMake(0,screen.searchDisplayController.searchBar.frame.size.height)
   end
+
+  it "should display a custom message when there are no results" do
+    table_screen = TableScreenSymbolSearchableNoResults.new
+    table_screen.on_load
+
+    table_screen.searchDisplayControllerWillBeginSearch(table_screen.searchDisplayController)
+    table_screen.searchDisplayController(table_screen.searchDisplayController, shouldReloadTableForSearchString:"supercalifragilisticexpialidocious")
+    table_screen.update_table_data
+
+    results_label = table_screen.searchDisplayController.searchResultsTableView.subviews.detect{|v| v.is_a?(UILabel)}
+    wait_for_change results_label, 'text' do
+      results_label.text.should == "Nada!"
+    end
+  end
 end

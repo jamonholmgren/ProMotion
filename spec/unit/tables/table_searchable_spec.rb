ac3a12df81b29eef0af20cc19a91c6e14895aff0
Andrew Havens <email@andrewhavens.com>
2018-04-02 23:46:24 -0700

    Update searchable table extension to support iOS 8 UISearchController.

--- spec/unit/tables/table_searchable_spec.rb
+++ spec/unit/tables/table_searchable_spec.rb
@@ -22,20 +22,6 @@ describe "table screen searchable functionality" do
     end
     screen = HiddenSearchScreen.new
     screen.on_load
-    screen.tableView.contentOffset.should == CGPointMake(0,screen.searchDisplayController.searchBar.frame.size.height)
-  end
-
-  it "should display a custom message when there are no results" do
-    table_screen = TableScreenSymbolSearchableNoResults.new
-    table_screen.on_load
-
-    table_screen.searchDisplayControllerWillBeginSearch(table_screen.searchDisplayController)
-    table_screen.searchDisplayController(table_screen.searchDisplayController, shouldReloadTableForSearchString:"supercalifragilisticexpialidocious")
-    table_screen.update_table_data
-
-    results_label = table_screen.searchDisplayController.searchResultsTableView.subviews.detect{|v| v.is_a?(UILabel)}
-    wait_for_change results_label, 'text' do
-      results_label.text.should == "Nada!"
-    end
+    screen.tableView.contentOffset.should == CGPointMake(0, screen.tableView.tableHeaderView.frame.size.height)
   end
 end
